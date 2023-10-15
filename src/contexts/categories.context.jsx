8911f4f16ac8e3a37fc46381cc7162506db410ae
Leslie Behum <llaurenb@gmail.com>
2023-10-15 15:36:51 -0400

    add more routing, categories

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState, useEffect} from "react";
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+// import SHOP_DATA from '../shop-data.js'
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+})
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+  
+// run once to import SHOP_DATA to firebase and create "categories". commented out so it will no run and attempt to write again 
+// useEffect(() => {
+  //   addCollectionAndDocuments('categroies'.SHOP_DATA);
+  // }, [])
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments()
+      console.log(categoryMap)
+      setCategoriesMap(categoryMap)
+    }
+    getCategoriesMap()
+  }, [])
+  
+  const value = { categoriesMap }
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  )
+
+}
